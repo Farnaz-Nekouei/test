beb56560b14da4d5d87a0d5d8f348f4ef6057ba2
Farnaz_Nekouei.S <farnaz.nk94@gmail.com>
2026-04-18 22:48:22 +0200

    delete to declott

--- streaming/producer.py
+++
@@ -1,6 +0,0 @@
-import time
-
-def stream(df):
-    for _, row in df.iterrows():
-        yield row.to_dict()
-        time.sleep(0.05)
--- twin/state.py
+++
@@ -1,5 +0,0 @@
-class State:
-    def __init__(self):
-        self.flow = 0
-        self.speed = 0
-        self.density = 0
